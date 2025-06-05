b83af03e0256792b53cb34e048257971b4c2f716
Bharg Patel <bharg.ranucle@outlook.com>
2025-06-05 16:29:24 +0530

    Add breadcrumbs in profile page.

--- app/admin/profile/page.tsx
+++ app/admin/profile/page.tsx
@@ -1,5 +1,6 @@
 import { ProfileForm } from "@/components/profile/profile-form"
 import { PageTitle } from "@/components/page-title"
+import { Breadcrumb } from "@/components/breadcrumb";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -9,8 +10,14 @@ export const metadata: Metadata = {
 };
 
 export default function ProfilePage() {
+
+  const breadcrumbs = [
+    { label: "Profile Settings", href: "#" },
+  ];
+
   return (
     <div className="space-y-6">
+        <Breadcrumb items={breadcrumbs} />
       <PageTitle title="Profile Settings" />
       <div className="rounded-xl border-none bg-white p-6 shadow-none dark:bg-background [box-shadow:0_8px_34px_rgba(0,0,0,0.1)]">
         <ProfileForm />
